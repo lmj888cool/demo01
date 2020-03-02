2268bab56b47c99eaa68b4aea16d53d316aa79ec
lmj888cool <lmj400@126.com>
2020-03-02 18:40:26 +0800

    君仔
    添加酒馆界面

--- Assets/Scripts/RolateObject.cs
+++ Assets/Scripts/RolateObject.cs
@@ -8,6 +8,7 @@ public class RolateObject : MonoBehaviour
     Vector3 beginTouchPos;//触点
     Vector3 beginTouchPosMoblie;//触点
     bool startRolateObject = false;
+    public bool isAuto = false;
     void Start()
     {
         beginTouchPos = Vector3.zero;
@@ -16,40 +17,49 @@ public class RolateObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /////////////////////////触摸屏////////////////////////
-#if UNITY_ANDROID || UNITY_IOS
-        if (Input.touchCount == 1) //[color=Red]如果点击手指touch了  并且手指touch的状态为移动的[/color]
+        if(!isAuto)
         {
-            if(Input.GetTouch(0).phase == TouchPhase.Moved)
+            /////////////////////////触摸屏////////////////////////
+#if UNITY_ANDROID || UNITY_IOS
+            if (Input.touchCount == 1) //[color=Red]如果点击手指touch了  并且手指touch的状态为移动的[/color]
             {
-                Vector3 touchposition = Input.GetTouch(0).position;  //[color=Red]获取手指touch最后一帧移动的xy轴距离[/color]
-                transform.Rotate(Vector3.up, beginTouchPosMoblie.x - touchposition.x);
+                if (Input.GetTouch(0).phase == TouchPhase.Moved)
+                {
+                    Vector3 touchposition = Input.GetTouch(0).position;  //[color=Red]获取手指touch最后一帧移动的xy轴距离[/color]
+                    transform.Rotate(Vector3.up, beginTouchPosMoblie.x - touchposition.x);
+                }
+                beginTouchPosMoblie = Input.GetTouch(0).position;
+
             }
-            beginTouchPosMoblie = Input.GetTouch(0).position;
-            
+            else
+            {
+                beginTouchPosMoblie = Vector3.zero;
+            }
+#endif
+            ////////////////////////////PC/////////////////////
+            if (Input.GetButtonUp("Fire1"))
+            {
+                startRolateObject = false;//松开鼠标左键
+            }
+
+            if (Input.GetButtonDown("Fire1"))//按下鼠标左键
+            {
+                startRolateObject = true;
+                beginTouchPos = Input.mousePosition;
+
+            }
+            if (startRolateObject)
+            {
+                print("nput.mousePosition.y - beginTouchPos.y:" + (Input.mousePosition.y - beginTouchPos.y));
+                transform.Rotate(Vector3.up, (beginTouchPos.x - Input.mousePosition.x) * 2.0f);
+                beginTouchPos = Input.mousePosition;
+            }
         }
         else
         {
-            beginTouchPosMoblie = Vector3.zero;
+            transform.Rotate(Vector3.up, 1.0f);
         }
-#endif
-        ////////////////////////////PC/////////////////////
-        if (Input.GetButtonUp("Fire1"))
-        {
-            startRolateObject = false;//松开鼠标左键
-        }
-        
-        if (Input.GetButtonDown("Fire1"))//按下鼠标左键
-        {
-            startRolateObject = true;
-            beginTouchPos = Input.mousePosition;
-            
-        }
-        if (startRolateObject) {
-            print("nput.mousePosition.y - beginTouchPos.y:" + (Input.mousePosition.y - beginTouchPos.y));
-            transform.Rotate(Vector3.up, (beginTouchPos.x-Input.mousePosition.x) * 2.0f);
-            beginTouchPos = Input.mousePosition;
-        }
+
     }
     private void OnDisable()
     {
---
+++ Assets/Scripts/Tavern/TavernScene.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TavernScene : MonoBehaviour
+{
+    Ray ray;
+    RaycastHit hit;
+    GameObject obj;
+    private Vector3 CameraDefaultPos;//摄像机初始的位置
+    public GameObject[] RandomHeroes;
+
+    public GameObject heroInfoPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CameraDefaultPos = Camera.main.transform.position;
+        OnClickBack();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("点击鼠标左键");
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.collider.gameObject.name);
+                obj = hit.collider.gameObject;
+                //通过名字
+                if (obj.name.Equals("BeiJiuChuan"))
+                {
+                    Debug.Log("点中" + obj.name);
+                }
+                //通过标签
+                if (obj.tag == "move")
+                {
+                    Debug.Log("点中" + obj.name);
+                }
+                for (int i = 0; i < RandomHeroes.Length; i++)
+                {
+                    if(RandomHeroes[i].gameObject.name == obj.name)
+                    {
+                        if (Camera.main != null)
+                        {
+                            iTween.MoveTo(Camera.main.gameObject, new Vector3(obj.transform.position.x, obj.transform.position.y + 0.12f, 7f), 0.5f);
+                            heroInfoPanel.gameObject.SetActive(true);
+                        }
+                    }
+                    else
+                    {
+                        RandomHeroes[i].gameObject.SetActive(false);
+                    }
+                }
+                
+            }
+        }
+
+    }
+    public void OnClickBack()
+    {
+        if (Camera.main != null)
+        {
+            iTween.MoveTo(Camera.main.gameObject, CameraDefaultPos, 0.5f);
+            heroInfoPanel.gameObject.SetActive(false);
+            for (int i = 0; i < RandomHeroes.Length; i++)
+            {
+                {
+                    RandomHeroes[i].gameObject.SetActive(true);
+                }
+            }
+            
+        }
+    }
+}
